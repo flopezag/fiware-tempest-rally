2735de01c7477d978181dd1c79ae72cb44deb67b
Jose Ignacio Carretero <joseignacio.carretero@gmail.com>
2017-07-24 10:25:23 +0000

    Implementation of https://jira.fiware.org/browse/OPS-1743 :wq  This is a configuration file which I can use to Store configuration information and configuration for   accessing the different regions and retrive their Tempest HTML oupt files.
    Implementation of https://jira.fiware.org/browse/OPS-1741
      This is changing the HTML in order to be able to show different regions.

---
+++ cfg/config.js
@@ -0,0 +1,12 @@
+var config = {} ;
+
+config.regions = {
+   'Spain': '/public/Spain.html',
+   'Brittany': '/public/Brittany.html',
+   'Lannion': '/public/Lannion.html'
+};
+
+config.port = 3000;
+config.listen = '0.0.0.0';
+
+module.exports = config;
